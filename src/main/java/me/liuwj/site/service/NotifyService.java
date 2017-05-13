a8d0ccd5e308b80d23b3b30fade8444955d2fa0d
Vincent Lau <me@liuwenjun.info>
2017-05-13 16:08:04 +0800

    log

--- src/main/java/me/liuwj/site/service/NotifyService.java
+++ src/main/java/me/liuwj/site/service/NotifyService.java
@@ -1,5 +1,6 @@
 package me.liuwj.site.service;
 
+import lombok.extern.slf4j.Slf4j;
 import me.liuwj.site.model.Comment;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by vince on 2017-05-13.
  */
+@Slf4j
 @Service
 public class NotifyService {
     @Autowired
@@ -23,6 +25,7 @@ public class NotifyService {
         messageToAdmin.setTo("me@liuwj.me");
         messageToAdmin.setSubject("Vincent's Site - 新评论提醒");
         messageToAdmin.setText("有新评论，点击查看：" + comment.getUrl());
+        log.info("sending mail: {}", messageToAdmin);
         mailSender.send(messageToAdmin);
 
         if (comment.getToUser() != null && StringUtils.isNotBlank(comment.getToUser().getEmail())) {
@@ -32,6 +35,7 @@ public class NotifyService {
             messageToUser.setSubject("Vincent's Site - 评论回复通知");
             messageToUser.setText(comment.getToUser().getName() + ", 您好：\n" +
                     comment.getFromUser() + " 回复了您的评论，点击查看：" + comment.getUrl());
+            log.info("sending mail: {}", messageToUser);
             mailSender.send(messageToUser);
         }
     }
