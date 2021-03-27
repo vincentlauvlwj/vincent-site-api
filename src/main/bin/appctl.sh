c665510462849a283fe3acf0042ca7fa74570ba8
vincent.liu <vincent.liu@lazada.com>
2021-03-27 17:03:29 +0800

    optimize start script

--- src/main/bin/appctl.sh
+++ src/main/bin/appctl.sh
@@ -17,29 +17,30 @@
 
 # custom settings...
 JAVA_OPTS="-XX:-UseGCOverheadLimit -XX:NewRatio=1 -XX:SurvivorRatio=8 -XX:+UseSerialGC"
-APP_MAINCLASS="com.tudaxia.test.TestMain"
+APP_MAINCLASS="me.liuwj.site.Application"
 
 # resolve links - $0 may be a softlink
 PRG="$0"
 
 while [ -h "$PRG" ]; do
-  ls=`ls -ld "$PRG"`
-  link=`expr "$ls" : '.*-> \(.*\)$'`
+  ls=$(ls -ld "$PRG")
+  link=$(expr "$ls" : '.*-> \(.*\)$')
   if expr "$link" : '/.*' > /dev/null; then
     PRG="$link"
   else
-    PRG=`dirname "$PRG"`/"$link"
+    PRG=$(dirname "$PRG")/"$link"
   fi
 done
 
-PRGDIR=`dirname "$PRG"`
-BASEDIR=`cd "$PRGDIR/.." >/dev/null; pwd`
+PRGDIR=$(dirname "$PRG")
+BASEDIR=$(cd "$PRGDIR/.." > /dev/null; pwd)
+PID_FILE="$BASEDIR/pid"
 
 
 # OS specific support.  $var _must_ be set to either true or false.
 cygwin=false;
 darwin=false;
-case "`uname`" in
+case "$(uname)" in
   CYGWIN*)
     cygwin=true
     ;;
@@ -52,7 +53,7 @@ case "`uname`" in
     fi
     if [ -z "$JAVA_HOME" ]; then
       if [ -x "/usr/libexec/java_home" ]; then
-        JAVA_HOME=`/usr/libexec/java_home`
+        JAVA_HOME=$(/usr/libexec/java_home)
       else
         JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/${JAVA_VERSION}/Home
       fi
@@ -62,14 +63,14 @@ esac
 
 if [ -z "$JAVA_HOME" ] ; then
   if [ -r /etc/gentoo-release ] ; then
-    JAVA_HOME=`java-config --jre-home`
+    JAVA_HOME=$(java-config --jre-home)
   fi
 fi
 
 # For Cygwin, ensure paths are in UNIX format before anything is touched
 if $cygwin ; then
-  [ -n "$JAVA_HOME" ] && JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
-  [ -n "$CLASSPATH" ] && CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
+  [ -n "$JAVA_HOME" ] && JAVA_HOME=$(cygpath --unix "$JAVA_HOME")
+  [ -n "$CLASSPATH" ] && CLASSPATH=$(cygpath --path --unix "$CLASSPATH")
 fi
 
 # If a specific java binary isn't specified search for the standard 'java' binary
@@ -82,7 +83,7 @@ if [ -z "$JAVACMD" ] ; then
       JAVACMD="$JAVA_HOME/bin/java"
     fi
   else
-    JAVACMD=`which java`
+    JAVACMD=$(which java)
   fi
 fi
 
@@ -93,11 +94,15 @@ if [ ! -x "$JAVACMD" ] ; then
 fi
 
 for i in "$BASEDIR"/lib/*.jar; do
+  if [ -n "$CLASSPATH" ]; then
     CLASSPATH="$CLASSPATH":"$i"
+  else
+    CLASSPATH="$i"
+  fi
 done
 
 if [ -n "$ENDORSED_DIR" ] ; then
-  CLASSPATH=$BASEDIR/$ENDORSED_DIR/*:$CLASSPATH
+  CLASSPATH=$BASEDIR/$ENDORSED_DIR:$CLASSPATH
 fi
 
 if [ -n "$CLASSPATH_PREFIX" ] ; then
@@ -106,34 +111,32 @@ fi
 
 # For Cygwin, switch paths to Windows format before running java
 if $cygwin; then
-  [ -n "$CLASSPATH" ] && CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
-  [ -n "$JAVA_HOME" ] && JAVA_HOME=`cygpath --path --windows "$JAVA_HOME"`
-  [ -n "$BASEDIR" ] && BASEDIR=`cygpath --path --windows "$BASEDIR"`
+  [ -n "$CLASSPATH" ] && CLASSPATH=$(cygpath --path --windows "$CLASSPATH")
+  [ -n "$JAVA_HOME" ] && JAVA_HOME=$(cygpath --path --windows "$JAVA_HOME")
+  [ -n "$BASEDIR" ] && BASEDIR=$(cygpath --path --windows "$BASEDIR")
 fi
 
 run() {
-  cd $BASEDIR
-  exec $JAVACMD $JAVA_OPTS -classpath $CLASSPATH $APP_MAINCLASS
+  cd "$BASEDIR"
+  exec "$JAVACMD" $JAVA_OPTS -classpath "$CLASSPATH" "$APP_MAINCLASS"
 }
 
 start() {
-  pid=`cat $PID_FILE`
-  if [ pid -gt 0 ]; then
+  if [ -s "$PID_FILE" ]; then
     echo "================================"
-    echo "Warn: $APP_MAINCLASS is already started! (pid=$pid)"
+    echo "Warn: Application $APP_MAINCLASS is already started! (pid=$(cat "$PID_FILE"))"
     echo "================================"
   else
-    echo -n "Starting $APP_MAINCLASS ..."
+    echo "Starting application $APP_MAINCLASS..."
     STDOUT=$BASEDIR/logs/service_stdout.log
-    [ -e $BASEDIR/logs ] || mkdir $BASEDIR/logs -p
+    [ -e "$BASEDIR"/logs ] || mkdir "$BASEDIR"/logs -p
 
-    cd $BASEDIR
-    nohup $JAVACMD $JAVA_OPTS -classpath $CLASSPATH $APP_MAINCLASS > $STDOUT 2>&1 &
-    echo $! > $PID_FILE
+    cd "$BASEDIR"
+    nohup "$JAVACMD" $JAVA_OPTS -classpath "$CLASSPATH" "$APP_MAINCLASS" > "$STDOUT" 2>&1 &
 
-    pid=`cat $PID_FILE`
-    if [ pid -gt 0 ]; then
-      echo "(pid=$pid) [OK]"
+    if [ $? -eq 0 ]; then
+      echo $! > "$PID_FILE"
+      echo "[OK] (pid=$(cat "$PID_FILE"))"
     else
       echo "[Failed]"
     fi
@@ -141,44 +144,43 @@ start() {
 }
 
 stop() {
-  pid=`cat $PID_FILE`
-  if [ pid -gt 0 ]; then
-    echo -n "Stopping $APP_MAINCLASS ...(pid=$pid) "
-    kill $pid
+  if [ -s "$PID_FILE" ]; then
+    echo "Stopping application $APP_MAINCLASS...(pid=$(cat "$PID_FILE"))"
+    kill "$(cat "$PID_FILE")"
 
     if [ $? -eq 0 ]; then
       echo "[OK]"
-      rm -f $PID_FILE
+      rm -f "$PID_FILE"
     else
       echo "[Failed]"
     fi
   else
     echo "================================"
-    echo "Warn: $APP_MAINCLASS is not running"
+    echo "Warn: Application $APP_MAINCLASS is not running"
     echo "================================"
   fi
 }
 
 status() {
-  pid=`cat $PID_FILE`
-  if [ pid -gt 0 ]; then
-    echo "$APP_MAINCLASS is running! (pid=$pid)"
+  if [ -s "$PID_FILE" ]; then
+    echo "Status: Application $APP_MAINCLASS is running! (pid=$(cat "$PID_FILE"))"
   else
-    echo "$APP_MAINCLASS is not running"
+    echo "Status: Application $APP_MAINCLASS is not running"
   fi
 }
 
 info() {
   echo "System Information:"
   echo "****************************"
-  echo `head -n 1 /etc/issue`
-  echo `uname -a`
+  echo "$(uname -a)"
   echo
   echo "JAVA_HOME=$JAVA_HOME"
-  echo `$JAVA_HOME/bin/java -version`
+  echo "$("$JAVACMD" -version)"
   echo
-  echo "APP_HOME=$APP_HOME"
+  echo "BASEDIR=$BASEDIR"
+  echo "JAVA_OPTS=$JAVA_OPTS"
   echo "APP_MAINCLASS=$APP_MAINCLASS"
+  echo "CLASSPATH=$CLASSPATH"
   echo "****************************"
 }
 
@@ -203,6 +205,6 @@ case "$1" in
     info
     ;;
   *)
-    echo "Usage: $0 {start|stop|restart|status|info}"
+    echo "Usage: $0 {run|start|stop|restart|status|info}"
     exit 1
- 
+esac
